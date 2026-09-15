0431316454c2e3d959994ed8c274ef5097743578
vcmselvin908-svg <vcmselvin908@gmail.com>
2026-09-15 15:37:06 +0530

    w

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -19,13 +19,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://personal-expense-tracker-pqgl-74j3ndpjf.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(auth.router)
 app.include_router(expenses.router)
